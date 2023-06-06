720a97d83a25d686c2a9ddb0d6929c7c63b445ec
Mathis Desbriere <mathis.desbriere@hotmail.fr>
2023-06-06 15:56:04 +1000

    Map et searchbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_05_071252) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_06_025731) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,7 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_05_071252) do
     t.float "longitude"
     t.float "latitude"
     t.bigint "trip_id", null: false
-    t.string "location"
+    t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["trip_id"], name: "index_markers_on_trip_id"
