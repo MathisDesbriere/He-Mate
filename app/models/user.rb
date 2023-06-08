23f0494d4feb2faeb331d1cfa936cd6f03277d72
NormanKu <kunorman4@gmail.com>
2023-06-08 12:46:14 +1000

    Comment feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_one_attached :image
   has_many :trips
+  has_many :comments
   validates :image, presence: true
 
 
---
+++ app/policies/comment_policy.rb
@@ -0,0 +1,13 @@
+class CommentPolicy < ApplicationPolicy
+  def create?
+    user.present?
+  end
+
+  def update?
+    user.present? && record.user_id == user.id
+  end
+
+  def destroy?
+    user.present? && record.user_id == user.id
+  end
+end
