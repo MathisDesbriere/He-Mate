ede6c7d38a114fc04675a9e4a37c770b0f59d081
alban avequin <avequin.alban@icloud.com>
2023-06-13 10:18:27 +1000

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,36 +11,37 @@ require 'open-uri'
 
 Activity.destroy_all
 Trip.destroy_all
+Marker.destroy_all
 
-# puts " creating users"
+# # puts " creating users"
 
-10.times do
-  User.create!(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    email: Faker::Internet.email,
-    password: Faker::Internet.password
-  )
-end
+# 10.times do
+#   User.create!(
+#     first_name: Faker::Name.first_name,
+#     last_name: Faker::Name.last_name,
+#     email: Faker::Internet.email,
+#     password: Faker::Internet.password
+#   )
+# end
 
-# puts "user's done"
-users = User.all
+# # puts "user's done"
+# users = User.all
 
-# Seed data for Trips
+# # Seed data for Trips
 
-5.times do
-  trip = Trip.new(
-    title: Faker::Lorem.word,
-    user: users.sample,
-    like: 10,
-    description: Faker::Lorem.paragraph
-  )
-  trip.user = users.sample
-  trip.save!
-end
+# 5.times do
+#   trip = Trip.new(
+#     title: Faker::Lorem.word,
+#     user: users.sample,
+#     like: 10,
+#     description: Faker::Lorem.paragraph
+#   )
+#   trip.user = users.sample
+#   trip.save!
+# end
 
 
-# Seed data for Activities
-trip = Trip.first
-Activity.create(trip: trip, title: "Activity 1", link: "https://example.com/activity1")
-Activity.create(trip: trip, title: "Activity 2", link: "https://example.com/activity2")
+# # Seed data for Activities
+# trip = Trip.first
+# Activity.create(trip: trip, title: "Activity 1", link: "https://example.com/activity1")
+# Activity.create(trip: trip, title: "Activity 2", link: "https://example.com/activity2")
